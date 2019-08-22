2f6742b6bc45a394ffe1fe9435d2456fef6275cb
Joseph Jabour <joseph.e.jabour@erdc.dren.mil>
2019-08-22 13:40:48 -0500

    Cleared master

--- bld.bat
+++
@@ -1,5 +0,0 @@
-git clone https://github.com/jejabour/stwavetesting.git
-git fetch
-git checkout -b stwave_32 origin/stwave_32
-python setup.py install --single-version-externally-managed --record=record.txt
-if errorlevel 1 exit 1
